3559f5f4040063e208f6ae8f12433614337439d1
jdcaperon <jack.d.caperon@gmail.com>
2019-08-11 12:57:27 +1000

    store bscl in database

--- stem-cell/src/actions/index.js
+++ stem-cell/src/actions/index.js
@@ -7,6 +7,20 @@ export function populateData() {
     return data;
 }
 
+export function updateUCE(name, uce) {
+    var query = databaseRef.orderByChild('name').equalTo(name)
+    query.once("child_added", function(snapshot) {
+        snapshot.ref.update({'uce': uce})
+    })
+}
+
+export function updateBSCL(name, bscl) {
+    var query = databaseRef.orderByChild('name').equalTo(name)
+    query.once("child_added", function(snapshot) {
+        snapshot.ref.update({'bscl': bscl})
+    })
+}
+
 export function addData(newData) {
   databaseRef.push().set(newData,
     function(error) {
@@ -22,14 +36,13 @@ export function fetchData() {
         var listOfData = []
         databaseRef.on('value', function(snapshot) {
             snapshot.forEach(function(childSnapshot) {
-                //console.log(listOfData);
+                console.log(childSnapshot.key);
                 listOfData.push({
                     "name": String(childSnapshot.val()["name"]),
                     "number": String(childSnapshot.val()["number"]),
                     "age": String(childSnapshot.val()["age"]),
                     "weight": String(childSnapshot.val()["weight"]),
-                    "calcium": String(childSnapshot.val()["calcium"]),
-
+                    "calcium": String(childSnapshot.val()["calcium"])
                 })
             });
         })
